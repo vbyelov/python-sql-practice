4ab478dd816df7ea41ddd3d590b5d197d177ab2e
V Byelov <vova.belov@gmail.com>
2026-02-05 10:06:46 +0100

    dictionaries

---
+++ python/50226dicts.py
@@ -0,0 +1,12 @@
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+phone_numbers = {'boss': 5551234567, 'Suzy': 22657854310}
+empty_dictionary = {}
+my_dict = {'seven': 7, 'eight': 8 , 'nine': 9, 'eleven': 11, 'twelve': 12}
+
+print(dictionary)
+print(phone_numbers)
+print(len(phone_numbers))
+print(empty_dictionary)
+
+for key in my_dict:
+    print(key, my_dict[key])
