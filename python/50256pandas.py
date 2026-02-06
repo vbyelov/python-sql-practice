4f71ba5a965fe3e08aa76fbf567bfe9f4ebd5866
V Byelov <vova.belov@gmail.com>
2026-02-06 13:23:50 +0100

    Pandas functions

--- python/50256pandas.py
+++ python/50256pandas.py
@@ -9,6 +9,7 @@ path = kagglehub.dataset_download("christopheiv/winemagdata130k")
 csv_path = path + "/winemag-data-130k-v2.csv"
 
 wine_reviews = pd.read_csv(csv_path, index_col=0)
+reviews = pd.read_csv(csv_path, index_col=0)
 
 pd.set_option('display.max_rows', 25)
 # print(wine_reviews.shape)
@@ -32,12 +33,31 @@ print(wine_reviews.iloc[0,1])
 #     ]
 # )
 
-print(wine_reviews.country == 'Italy')
+# print(wine_reviews.country == 'Italy')
+#
+# print(wine_reviews.loc[(wine_reviews.country == 'Italy') & (wine_reviews.points >= 90)])
+#
+# print(wine_reviews.loc[(wine_reviews.country == 'Italy') | (wine_reviews.points >= 90)])
+#
+# print(wine_reviews.loc[wine_reviews.country.isin(['Italy', 'France'])])
 
-print(wine_reviews.loc[(wine_reviews.country == 'Italy') & (wine_reviews.points >= 90)])
+# print(wine_reviews.loc[wine_reviews.price.isnull()])
 
-print(wine_reviews.loc[(wine_reviews.country == 'Italy') | (wine_reviews.points >= 90)])
+print(reviews.points.describe())
+print(reviews.country.describe())
+print(reviews.designation.unique())
+print(reviews.country.value_counts())
 
-print(wine_reviews.loc[wine_reviews.country.isin(['Italy', 'France'])])
+reviews['points_centered'] = reviews.points - reviews.points.mean()
+reviews['points_median'] = reviews.points.median()
 
-print(reviews.loc[reviews.price.notnull()])
+print(reviews[['points', 'points_centered', 'points_median']].head())
+
+def preobrazovator(points):
+    points = points/100
+    return points
+
+reviews['new_colomn'] = reviews.points.map(preobrazovator)
+
+print(reviews.new_colomn.describe())
+print(reviews.new_colomn.head())
