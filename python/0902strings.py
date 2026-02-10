6a4d6dcfacd56a636adfee5b6fed45dea738521f
V Byelov <vova.belov@gmail.com>
2026-02-10 09:25:53 +0100

    strings and random function

---
+++ python/0902strings.py
@@ -0,0 +1,22 @@
+import random
+import string
+import choice
+import time
+ALPHABET = string.ascii_letters + string.digits
+
+print(ALPHABET)
+
+def random_string(length):
+    random_string = ''
+    for i in range(length):
+        random_string += random.choice(ALPHABET)
+    return random_string
+
+while True:
+    rand_choice = random.choice(range(10,20))
+    rand_string = random_string(rand_choice)
+    print(rand_choice, ' letters->', rand_string)
+    time.sleep(1)
+
+
+
