b043aee39daf2fa19784bc4583e3663f3cd9fcc0
V Byelov <vova.belov@gmail.com>
2026-02-12 09:39:01 +0100

    math library

--- python/math_lib.py
+++ python/math_lib.py
@@ -1,10 +1,42 @@
-import math, sys
 
-math.pi
-y = math.sin(math.pi/2)
+import math as mate, os as o, time as t
+import os
 
-print(y)
+from math import pi as PI, sin as SIN, sqrt as SQRT
 
-for x in range(360):
-    print(math.sin(x))
-    print("")
+print (SQRT(SIN(7)))
+
+t.sleep(1)
+print(os.environ)
+
+for i in os.environ:
+    print(i)
+
+print(mate.pi)
+print(mate.e)
+
+
+from math import pi, sin
+pi
+# y = sin(math.pi/2)
+
+# print(y)
+
+# for x in range(360):
+#     print(math.sin(x))
+#     print("")
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+pi = 3.1415
+
+print(sin(pi/2))
+print(sin(pi/2))
+
+
