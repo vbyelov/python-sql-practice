5ba5a50cac1249d9ceba9f102eef01b0ffa5e6ad
V Byelov <vova.belov@gmail.com>
2026-02-14 14:02:08 +0100

    platform functions

--- python/1402libraries.py
+++ python/1402libraries.py
@@ -1,4 +1,5 @@
-from platform import platform, machine, processor
+from platform import platform, machine, processor, system, version
+from platform import python_implementation, python_version_tuple
 
 print(platform(aliased = False, terse = False))
 print(platform())
@@ -8,3 +9,15 @@ print(platform(0, 1))
 print(machine())
 
 print(processor())
+print(system() + ' System')
+print(version() + ' Version')
+
+print(python_implementation() + ' Python Implementation')
+print(str(python_version_tuple()) + ' Python Version Tuple')
+
+for atr in python_version_tuple():
+    print(atr)
+
+import platform
+
+print(dir(platform))
