2ad42053936fc941a5e3a0ff3c3a6b8049dff716
V Byelov <vova.belov@gmail.com>
2026-02-16 09:37:16 +0100

    modules

--- python/import_mod.py
+++ python/import_mod.py
@@ -1,3 +1,14 @@
-import module
+# import module
 
-print(module.counter)
+from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(suml(zeroes))
+print(prodl(ones))
+
+import sys
+
+for p in sys.path:
+    print(p)
+
--- python/module.py
+++ python/module.py
@@ -1,9 +1,41 @@
+#!/usr/bin/env python3
 
-counter = 0
+""" module.py - an example of a Python module """
+
+
+
+# counter = 0
 
 print("Inside a module!")
 print('-----------------')
 print(__name__)
+
+#!/usr/bin/env python3
+
+""" module.py - an example of a Python module """
+
+__counter = 0
+
+
+def suml(the_list):
+    global __counter
+    __counter += 1
+    the_sum = 0
+    for element in the_list:
+        the_sum += element
+    return the_sum
+
+
+def prodl(the_list):
+    global __counter
+    __counter += 1
+    prod = 1
+    for element in the_list:
+        prod *= element
+    return prod
+
+
+
 
 if __name__ == '__main__':
     print("This is Main Script")
@@ -11,3 +43,9 @@ elif __name__ == 'module':
     print("Inside a module!")
 else:
     print("Inside a function!")
+
+my_list = [i+1 for i in range(5)]
+print(suml(my_list) == 15)
+print(__counter)
+print(prodl(my_list) == 120)
+print(__counter)
