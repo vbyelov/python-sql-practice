5d56869513dc6f3bbb44c7b4905f93338fd7e9a5
V Byelov <vova.belov@gmail.com>
2026-02-24 08:29:19 +0100

    strings min, max, list, index, count

--- python/strings2.py
+++ python/strings2.py
@@ -29,12 +29,45 @@ print(mylist)
 for numbers in mylist:
     print(int(numbers)*7)
 
-mylist1 = list('This is a very long string')
+mylist1 = list('''
+I can put many many lines here.
+From the very beginning till the end of file
+More and more
+This is a very long string
+'''
+               )
 print(mylist1)
 mylist1 = mylist1[::-1]
 print(mylist1)
 
 print(mylist1.count('l'))
 print(mylist1.count(' '))
+print(mylist1.count('\n'))
+
+#Capitalize
+
+print("Alpha".capitalize())
+print('ALPHA'.capitalize())
+print(' Alpha'.capitalize())
+print('123'.capitalize())
+print("αβγδ".capitalize())
 
 
+# Demonstrating the center() method:
+print('[' + 'alpha'.center(10) + ']')
+print('[' + 'Beta'.center(2) + ']')
+print('[' + 'Beta'.center(4) + ']')
+print('[' + 'Beta'.center(60) + ']')
+print('[' + 'gamma'.center(20, '%') + ']')
+
+# Demonstrating the endswith() method:
+if "the great epsilon".endswith("on"):
+    print("yes")
+else:
+    print("no")
+
+t = "zeta"
+print(t.endswith("a"))
+print(t.endswith("A"))
+print(t.endswith("et"))
+print(t.endswith("eta"))
