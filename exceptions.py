07c5e2b03f30558f0243be780ebe8cabc469e187
V Byelov <vova.belov@gmail.com>
2026-03-03 08:15:00 +0100

    exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,21 @@
-from math import tan, radians
-angle = int(input('Enter integral angle in degrees: '))
+# from math import tan, radians
+# angle = int(input('Enter integral angle in degrees: '))
+#
+# # We must be sure that angle != 90 + k * 180
+# assert angle % 180 != 90
+# print(tan(radians(angle)))
 
-# We must be sure that angle != 90 + k * 180
-assert angle % 180 != 90
-print(tan(radians(angle)))
+
+
+list = [1, 2, 3, 4, 5]
+indx = 0
+flag = True
+
+while flag:
+    try:
+        print(list[indx])
+        indx += 1
+    except IndexError:
+        flag = False
+
+print("Out of loop")
