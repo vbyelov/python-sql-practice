3cd11c937a735a9987bd5de13c3dd1224f5e8da5
V Byelov <vova.belov@gmail.com>
2026-03-10 08:31:56 +0100

    Fifo stack class, with exception

--- python/classes/FIFO_stack.py
+++ python/classes/FIFO_stack.py
@@ -13,6 +13,26 @@ class Queue:
             raise QueueError("Queue is empty")
         else:
             return self.__queue.pop()
+class Extended(Queue):
+    def is_empty(self):
+        try:
+            item = self.get()
+            self.put(item)
+            return False
+        except QueueError:
+            return True
+
+
+
+que = Extended()
+que.put(1)
+que.put("dog")
+que.put(False)
+for i in range(4):
+    if not que.is_empty():
+        print(que.get())
+    else:
+        print("Queue empty")
 
 
 que = Queue()
