7bab2fcdf01cd9aca3bb2e09c5e091e5356a0dbc
V Byelov <vova.belov@gmail.com>
2026-03-11 07:57:26 +0100

    __dict__

--- python/classes/instance_vars.py
+++ python/classes/instance_vars.py
@@ -1,22 +1,74 @@
-class ExampleClass:
-    def __init__(self, val = 1):
-        self.first = val
+# class ExampleClass:
+#     def __init__(self, val = 1):
+#         self.first = val
+#
+#     def set_second(self, val):
+#         self.second = val
+#
+#
+# example_object_1 = ExampleClass()
+# example_object_2 = ExampleClass(2)
+#
+# example_object_2.set_second(3)
+#
+# example_object_3 = ExampleClass(4)
+# example_object_3.third = {'ABC': 'ERD'}
+#
+# print(example_object_1.__dict__)
+# print(example_object_2.__dict__)
+# print(example_object_3.__dict__)
+#
+# for key, value in example_object_3.__dict__.items():
+#     print(key, '--' ,value)
+#
+#
+# class ExampleClass:
+#     def __init__(self, val = 1):
+#         self.__first = val
+#
+#     def set_second(self, val = 2):
+#         self.__second = val
+#
+#
+# example_object_1 = ExampleClass()
+# example_object_2 = ExampleClass(2)
+#
+# example_object_2.set_second(3)
+#
+# example_object_3 = ExampleClass(4)
+# example_object_3.__third = 5
+#
+#
+# print(example_object_1.__dict__)
+# print(example_object_2.__dict__)
+# print(example_object_3.__dict__)
 
-    def set_second(self, val):
-        self.second = val
+class ExampleClass:
+    counter = 0
+    def __init__(self, val = 1):
+        self.__first = val
+        ExampleClass.counter += 1
 
 
 example_object_1 = ExampleClass()
 example_object_2 = ExampleClass(2)
-
-example_object_2.set_second(3)
-
 example_object_3 = ExampleClass(4)
-example_object_3.third = {'ABC': 'ERD'}
+example_object_4 = ExampleClass(5)
 
-print(example_object_1.__dict__)
-print(example_object_2.__dict__)
-print(example_object_3.__dict__)
+print(example_object_1.__dict__, example_object_1.counter)
+print(example_object_2.__dict__, example_object_2.counter)
+print(example_object_3.__dict__, example_object_3.counter)
 
-for key, value in example_object_3.__dict__.items():
-    print(key, '--' ,value)
+
+class ExampleClass:
+    varia = 1
+    def __init__(self, val):
+        ExampleClass.varia = val
+
+
+print(ExampleClass.__dict__)
+example_object = ExampleClass(2)
+
+print(ExampleClass.__dict__)
+print(example_object.__dict__)
+
