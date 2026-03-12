1e90cfb1e048c95f6fd403c275fba58800312988
V Byelov <vova.belov@gmail.com>
2026-03-12 07:46:11 +0100

    methods

--- python/classes/method1.py
+++ python/classes/method1.py
@@ -1,6 +1,34 @@
+
 class Classy:
+    def method(self, par):
+        print("method:", par)
+
+
+obj = Classy()
+obj.method(1)
+obj.method(2)
+obj.method(3)
+
+obj2 = Classy()
+obj2.method((1,7,7))
+
+class Classy2:
+    varia = 2
+    def method(self):
+        print(self.varia, self.var)
+
+
+obj = Classy2()
+obj.var = 3
+obj.method()
+
+class Classy:
+    def other(self):
+        print("other")
+
     def method(self):
         print("method")
+        self.other()
 
 
 obj = Classy()
