4a3951f0d610ac16bc42a7de245cea5b141a94ab
V Byelov <vova.belov@gmail.com>
2026-03-17 08:19:09 +0100

    Triangle

--- python/classes/triangle.py
+++ python/classes/triangle.py
@@ -25,9 +25,13 @@ class Triangle:
     def __init__(self, vertice1, vertice2, vertice3):
         self.__points = [vertice1, vertice2, vertice3]
 
+    def __str__(self):
+        return 'This is a Triangle class with %d vertices' % len(self.__points)
+
     def perimeter(self):
         p1, p2, p3 = self.__points
         return (p1.distance_from_point(p2) + p2.distance_from_point(p3) + p3.distance_from_point(p1))
 
 triangle = Triangle(Point(0,0), Point(1,0), Point(0,1))
 print(triangle.perimeter())
+print(triangle.__str__())
