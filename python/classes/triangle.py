82f6d2875523096c23fbfd37507e70792a4dd640
V Byelov <vova.belov@gmail.com>
2026-03-17 08:15:47 +0100

    Triangle

--- python/classes/triangle.py
+++ python/classes/triangle.py
@@ -1,3 +1,5 @@
+import math
+
 class Point:
     def __init__(self, x=0.0, y=0.0):
         self.__x = x
@@ -10,8 +12,8 @@ class Point:
        return self.__y
 
     def distance_from_xy(self, x, y):
-        dx = x - self.__x - x
-        dy = y - self.__y - y
+        dx = self.__x - x
+        dy = self.__y - y
         return math.hypot(dx, dy)
 
     def distance_from_point(self, point):
@@ -27,5 +29,5 @@ class Triangle:
         p1, p2, p3 = self.__points
         return (p1.distance_from_point(p2) + p2.distance_from_point(p3) + p3.distance_from_point(p1))
 
-    triangle = Triangle(Point(0,0), Point(1,0), Point(0,1))
-    print(triangle.perimeter())
+triangle = Triangle(Point(0,0), Point(1,0), Point(0,1))
+print(triangle.perimeter())
