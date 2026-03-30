ef811fd9f2a55a107e97e09029e553e296b2963d
V Byelov <vova.belov@gmail.com>
2026-03-30 09:36:44 +0200

    exception with else block and finally block

--- python/classes/else_exception.py
+++ python/classes/else_exception.py
@@ -1,11 +1,26 @@
+# def reciprocal(n):
+#     try:
+#         n = 1 / n
+#     except ZeroDivisionError:
+#         print("Division failed")
+#         return None
+#     else:
+#         print("Everything went fine")
+#         return n
+#
+#
+# print(reciprocal(2))
+# print(reciprocal(0))
 def reciprocal(n):
     try:
         n = 1 / n
     except ZeroDivisionError:
         print("Division failed")
-        return None
+        n = None
     else:
         print("Everything went fine")
+    finally:
+        print("It's time to say goodbye")
         return n
 
 
