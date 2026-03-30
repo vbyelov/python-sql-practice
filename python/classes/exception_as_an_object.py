53e0e5d2ceeb5f7ccd9127ac0602802346218843
V Byelov <vova.belov@gmail.com>
2026-03-30 09:47:43 +0200

    exception as an object

--- python/classes/exception_as_an_object.py
+++ python/classes/exception_as_an_object.py
@@ -1,5 +1,27 @@
+def print_args(args):
+    lng = len(args)
+    if lng == 0:
+        print("")
+    elif lng == 1:
+        print(args[0])
+    else:
+        print(str(args))
+
+
 try:
-    i = int("Seieben")
+    raise Exception
 except Exception as e:
-    print(e)
-    print(e.__str__())
+    print(e, e.__str__(), sep=' : ' ,end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my", "exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
