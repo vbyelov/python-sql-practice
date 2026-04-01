2cb136a5b0624a3e908bf80cb7bbef3897c9a57c
V Byelov <vova.belov@gmail.com>
2026-04-01 10:33:47 +0200

    yield, list comprehensions

--- python/misc_tools_libraries/generators.py
+++ python/misc_tools_libraries/generators.py
@@ -75,5 +75,16 @@ def powers_of_2(n):
         power *= 2
 
 
-for v in powers_of_2(4)
+for v in powers_of_2(4):
     print(v)
+
+def powers_of_2(n):
+    power = 1
+    for i in range(n):
+        yield power
+        power *= 2
+
+
+t = [x for x in powers_of_2(5)]
+print(t)
+
