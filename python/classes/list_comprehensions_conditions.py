d9196336667cc8321f6e55887708aaf0c881beff
V Byelov <vova.belov@gmail.com>
2026-04-07 09:23:52 +0200

    generators and list comprehensions

--- python/classes/list_comprehensions_conditions.py
+++ python/classes/list_comprehensions_conditions.py
@@ -8,3 +8,14 @@ print(the_list)
 the_list = [0 if x % 2 != 0 else 1 for x in range(10)]
 
 print(the_list)
+
+the_list = [1 if x % 2 == 0 else 0 for x in range(10)]
+the_generator = (1 if x % 2 == 0 else 0 for x in range(10))
+
+for v in the_list:
+    print(v, end=" ")
+print()
+
+for v in the_generator:
+    print(v, end=" ")
+print()
