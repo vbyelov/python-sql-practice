84adec26473e530ffefa019fe40c6b5e2efb8a8c
V Byelov <vova.belov@gmail.com>
2026-04-07 09:38:58 +0200

    lamda function

--- python/classes/lambda_func.py
+++ python/classes/lambda_func.py
@@ -5,3 +5,11 @@ for x in range(10, 20):
     print(x, opposite_number(x))
     print()
     print(pwr_third(x))
+
+
+def print_function(args, fun):
+    for x in args:
+        print('f(', x,')=', fun(x), sep='')
+
+print_function([x for x in range(-2, 3)], lambda x: 2 * x**2 - 4 * x + 2)
+
