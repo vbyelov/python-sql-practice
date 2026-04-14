0cecda29a3491e54832fe826566dc9285832f357
V Byelov <vova.belov@gmail.com>
2026-04-14 08:33:26 +0200

    files and access errors

--- python/working_with_files/files.py
+++ python/working_with_files/files.py
@@ -1,8 +1,23 @@
 try:
-    stream = open("file2.txt", "rt")
+    stream = open("file.txt", "rt")
     # Processing goes here.
     stream.close()
 except Exception as exc:
     print("Cannot open the file:", exc)
     print(exc.errno)
     print(exc)
+
+
+import errno
+
+try:
+    s = open("file.txt", "rt")
+    # Actual processing goes here.
+    s.close()
+except Exception as exc:
+    if exc.errno == errno.ENOENT:
+        print("The file doesn't exist.")
+    elif exc.errno == errno.EMFILE:
+        print("You've opened too many files.")
+    else:
+        print("The error number is:", exc.errno)
