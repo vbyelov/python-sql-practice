9cf13b1db17d0a8bcde4c3f240319e9a98240c38
V Byelov <vova.belov@gmail.com>
2026-04-14 08:29:22 +0200

    files and access errors

--- python/working_with_files/files.py
+++ python/working_with_files/files.py
@@ -1,8 +1,9 @@
 try:
-    stream = open("file.txt", "rt")
+    stream = open("fi1le.txt", "rt")
     # Processing goes here.
     stream.close()
 except Exception as exc:
     print("Cannot open the file:", exc)
     print(exc.errno)
-    exit(1)
+    for i in exc:
+        print(i)
