d5d67f6f6d29acf7038bb845145ba5cbe3455786
V Byelov <vova.belov@gmail.com>
2026-04-15 09:10:26 +0200

    reading file, count

--- python/working_with_files/reading.py
+++ python/working_with_files/reading.py
@@ -14,3 +14,19 @@ with open("tzop.txt", "rt", encoding="utf-8") as stream:
 
 print("length:", len(content))
 print("Inhalt:", content)
+
+
+from os import strerror
+
+try:
+    cnt = 0
+    s = open('text.txt', "rt")
+    ch = s.read(1)
+    while ch != '':
+        print(ch, end='')
+        cnt += 1
+        ch = s.read(1)
+    s.close()
+    print("\n\nCharacters in file:", cnt)
+except IOError as e:
+    print("I/O error occurred: ", strerror(e.errno))
