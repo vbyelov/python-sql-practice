a9004b3aa2a67fc9ebd2b3050ba7d48e75a0290b
V Byelov <vova.belov@gmail.com>
2026-04-15 08:24:13 +0200

    files and access errors

---
+++ python/working_with_files/reading.py
@@ -0,0 +1,16 @@
+import os
+#Opening tzop.txt in read mode, returning it as a file object:
+stream = open("tzop.txt", "rt", encoding="utf-8")
+
+content = stream.read()
+
+print("=== START ===")
+print(content)
+print("=== END ===")
+
+
+with open("tzop.txt", "rt", encoding="utf-8") as stream:
+    content = stream.read()
+
+print("Длина:", len(content))
+print("Контент:", content)
