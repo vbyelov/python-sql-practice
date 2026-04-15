d1c326cf5cf87b7483e3032f6446541d5e6e0b96
V Byelov <vova.belov@gmail.com>
2026-04-15 09:14:07 +0200

    reading file, count, readline

--- python/working_with_files/reading.py
+++ python/working_with_files/reading.py
@@ -30,3 +30,23 @@ try:
     print("\n\nCharacters in file:", cnt)
 except IOError as e:
     print("I/O error occurred: ", strerror(e.errno))
+
+# Readline
+
+from os import strerror
+
+try:
+    ccnt = lcnt = 0
+    s = open('text.txt', 'rt')
+    line = s.readline()
+    while line != '':
+        lcnt += 1
+        for ch in line:
+            print(ch, end='')
+            ccnt += 1
+        line = s.readline()
+    s.close()
+    print("\n\nCharacters in file:", ccnt)
+    print("Lines in file:     ", lcnt)
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
