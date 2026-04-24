9d3c98835e3a052bc425bcbb98c30f54dec81101
V Byelov <vova.belov@gmail.com>
2026-04-24 10:08:58 +0200

    os module

--- python/os/osmodule.py
+++ python/os/osmodule.py
@@ -1,7 +1,7 @@
 import os
-print(os.uname())
 
-import os
-
-os.mkdir("my_first_directory")
-print(os.listdir())
+os.makedirs("my_first_directory/my_second_directory")
+os.chdir("my_first_directory")
+print(os.getcwd())
+os.chdir("my_second_directory")
+print(os.getcwd())
