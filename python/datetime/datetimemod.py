4995591b2f863512e9421d89259fa78248daccef
V Byelov <vova.belov@gmail.com>
2026-04-27 08:37:16 +0200

    daytime

--- python/datetime/datetimemod.py
+++ python/datetime/datetimemod.py
@@ -32,3 +32,14 @@ from datetime import date
 
 d = date(2019, 11, 7)
 print(d.weekday())
+
+
+from datetime import time
+
+t = time(14, 53, 20, 1)
+
+print("Time:", t)
+print("Hour:", t.hour)
+print("Minute:", t.minute)
+print("Second:", t.second)
+print("Microsecond:", t.microsecond)
