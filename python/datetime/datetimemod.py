05ae06113cf9011b334f6aa9f47e127391ec7714
V Byelov <vova.belov@gmail.com>
2026-04-28 08:19:03 +0200

    daytime

--- python/datetime/datetimemod.py
+++ python/datetime/datetimemod.py
@@ -65,3 +65,11 @@ import time
 timestamp = 1572879180
 print(time.gmtime(timestamp))
 print(time.localtime(timestamp))
+
+
+timestamp = 1572879180
+st = time.gmtime(timestamp)
+
+print(time.asctime(st))
+print(time.mktime((2019, 11, 4, 14, 53, 0, 0, 308, 0)))
+
