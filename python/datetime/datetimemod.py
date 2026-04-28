24def1779a6124024f1fd4d837395e2eaf5c49ad
V Byelov <vova.belov@gmail.com>
2026-04-28 08:14:09 +0200

    daytime

--- python/datetime/datetimemod.py
+++ python/datetime/datetimemod.py
@@ -43,3 +43,14 @@ print("Hour:", t.hour)
 print("Minute:", t.minute)
 print("Second:", t.second)
 print("Microsecond:", t.microsecond)
+
+import time
+
+class Student:
+    def take_nap(self, seconds):
+        print("I'm very tired. I have to take a nap. See you later.")
+        time.sleep(seconds)
+        print("I slept well! I feel great!")
+
+student = Student()
+student.take_nap(5)
