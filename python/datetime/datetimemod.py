18fa34e9b7b877f8b52d27a98199f42bc582c269
V Byelov <vova.belov@gmail.com>
2026-04-30 08:25:29 +0200

    daytime

--- python/datetime/datetimemod.py
+++ python/datetime/datetimemod.py
@@ -118,3 +118,12 @@ print("Days:", delta.days)
 print("Seconds:", delta.seconds)
 print("Microseconds:", delta.microseconds)
 
+
+from datetime import timedelta
+
+delta = timedelta(weeks=2, days=2, hours=3)
+print("Days:", delta.days)
+print("Seconds:", delta.seconds)
+print("Microseconds:", delta.microseconds)
+
+
