07faa6ef55c786a3c78f5b0131ecf4e77d430502
V Byelov <vova.belov@gmail.com>
2026-05-01 12:51:20 +0200

    calendar

--- python/datetime/calendr.py
+++ python/datetime/calendr.py
@@ -11,5 +11,5 @@ print(calendar.month(2026, 5))
 calendar.setfirstweekday(5)
 calendar.prmonth(2020, 12)
 
-print(calendar.isleap(2020))
+print(calendar.isleap(2027))
 print(calendar.leapdays(2010, 6021))
