ab7bfc2ea256cfd67a7ff7a7920aa45f18643317
V Byelov <vova.belov@gmail.com>
2026-05-12 08:50:31 +0200

    calendar iterators

--- python/datetime/iterdays.py
+++ python/datetime/iterdays.py
@@ -4,3 +4,11 @@ c = calendar.Calendar()
 
 for date in c.itermonthdates(2019, 11):
     print(date, end=" ")
+
+
+import calendar
+
+c = calendar.Calendar()
+
+for iter in c.itermonthdates4(2019, 11):
+    print(iter, end=" ")
